2c962a6abe701e2227c66b542180034543cdf2d0
seth <sethalexander@protonmail.com>
2017-04-14 15:32:56 -0500

    added bootstrap navbar to layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::Twitter::Bootstrap::Assets
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "my_super_secret_session"
+  end
+
+  get '/' do
+    erb :'index'
   end
 
 end
