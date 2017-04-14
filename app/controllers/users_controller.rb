f0944f3badf196cad2f370e6dac5680b6a1463d5
seth <sethalexander@protonmail.com>
2017-04-14 17:29:18 -0500

    added signup controllers & view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,37 @@ class UsersController < ApplicationController
 
   get '/login' do
     if is_logged_in?(session)
-      "You're logged in"
+      redirect 'houses'
     else
       erb :'users/login'
     end
   end
 
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if !!@user
+      session[:id] = @user[:id]
+      redirect 'houses'
+    else
+      redirect 'login'
+    end
+  end
+
   get '/signup' do
     if is_logged_in?(session)
-      "You're logged in"
+      redirect 'houses'
     else
       erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save
+      session[:id] = @user.id
+      redirect 'tweets'
+    else
+      redirect 'signup'
     end
   end
 
