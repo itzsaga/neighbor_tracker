220a205fcd1014878c1ddad5a113e26f4d40a4a2
seth <sethalexander@protonmail.com>
2017-04-14 13:46:22 -0500

    initial controller build

--- db/migrate/20170414141614_create_children.rb
+++ db/migrate/20170414141614_create_children.rb
@@ -2,7 +2,7 @@ class CreateChildren < ActiveRecord::Migration[5.0]
   def change
     create_table :children do |t|
       t.string :name
-      t.age :integer
+      t.integer :age
     end
   end
 end
