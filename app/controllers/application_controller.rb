451dccb16c099169c2ec6b3d6e02d7ea3bf1f3c5
seth <sethalexander@protonmail.com>
2017-04-14 15:54:03 -0500

    added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,21 @@
-require './config/environment'
-
 class ApplicationController < Sinatra::Base
-  register Sinatra::Twitter::Bootstrap::Assets
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "my_super_secret_session"
+  end
+
+  helpers do
+    def current_user(session)
+      @user = User.find(session[:id])
+      @user
+    end
+
+    def is_logged_in?(session)
+      !!session[:id] ? true : false
+    end
   end
 
   get '/' do
