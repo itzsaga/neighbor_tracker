488c03e7dd84eafe6c56103e66074be794e91e79
seth <sethalexander@protonmail.com>
2017-04-18 13:52:19 -0500

    index route working now

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -2,7 +2,12 @@ class ParentsController < ApplicationController
 
   get '/parents' do
     if is_logged_in?(session)
-      @parents = current_user(session).houses
+      @parents = []
+      current_user(session).houses.each do |house|
+        Parent.where(house_id: house.id).each do |parent|
+          @parents << parent
+        end
+      end
       erb :'parents/parents'
     else
       erb :'error'
