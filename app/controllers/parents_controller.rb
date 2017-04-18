b98689172f33262b5b11292ac3bce107337ffdcb
seth <sethalexander@protonmail.com>
2017-04-18 17:00:12 -0500

    used current_user_parents method

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -2,12 +2,7 @@ class ParentsController < ApplicationController
 
   get '/parents' do
     if is_logged_in?(session)
-      @parents = []
-      current_user(session).houses.each do |house|
-        Parent.where(house_id: house.id).each do |parent|
-          @parents << parent
-        end
-      end
+      @parents = current_user_parents
       erb :'parents/parents'
     else
       erb :'error'
