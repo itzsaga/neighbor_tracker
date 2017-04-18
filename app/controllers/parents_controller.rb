0a36de309f3f6a8d18a0bb8399eb7204f65872c1
seth <sethalexander@protonmail.com>
2017-04-18 11:56:47 -0500

    added parents route

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,3 +1,12 @@
 class ParentsController < ApplicationController
 
+  get '/parents' do
+    if is_logged_in?(session)
+      @parents = current_user(session).houses
+      erb :'parents/parents'
+    else
+      erb :'error'
+    end
+  end
+
 end
