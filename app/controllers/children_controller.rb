4ffc1d205a683e015ce7534d31e694c8f819d942
seth <sethalexander@protonmail.com>
2017-04-18 17:00:28 -0500

    added children route

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,3 +1,12 @@
 class ChildrenController < ApplicationController
 
+  get '/children' do
+    if is_logged_in?(session)
+      @parents = current_user_parents
+      erb 'children/children'
+    else
+      erb :'error'
+    end
+  end
+
 end
