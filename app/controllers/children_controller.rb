19da20cb6d9cfd86dc4fe9ce9b43e21b3c3996ba
seth <sethalexander@protonmail.com>
2017-04-18 17:11:46 -0500

    children index updated with ages

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -3,7 +3,8 @@ class ChildrenController < ApplicationController
   get '/children' do
     if is_logged_in?(session)
       @parents = current_user_parents
-      erb 'children/children'
+      @children = @parents.collect {|parent| parent.children}.flatten
+      erb :'children/children'
     else
       erb :'error'
     end
